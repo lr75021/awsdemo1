81ea55d0df5676f8f006473abb25a0f5cbdcbe91
lr75021 <lr75021@gmail.com>
2017-03-17 12:05:51 -0700

    change as v1.0

--- test1.js
+++ test1.js
@@ -1,19 +1,9 @@
 var http = require('http');
 var port = 9000;
-
-
-
-
-
 		
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type':  'text/plain'});
-
-
-
-
-	res.end('Hello world, start node.js, this will be fun\n');
-
+	res.end('Hello world, start node.js, this will be fun - V1.0\n');
 }).listen(port);
 
 console.log('Listening on port', port);
