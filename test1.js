8163983f2e320d4762a20ce64f427dd376c84366
lr75021 <lr75021@gmail.com>
2017-03-17 11:58:42 -0700

    change hello

--- test1.js
+++ test1.js
@@ -3,7 +3,7 @@ var port = 9000;
 		
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type':  'text/plain'});
-	res.end('Hello world, let\'s start node.js\n');
+	res.end('Hello world, start node.js, this will be fun\n');
 }).listen(port);
 		
 console.log('Listening on port', port);
